8d6bce553f6f1539c2b91f2ba6e7d440ea35bbe8
Delek Turner <dturner@victoria.ca>
2015-05-11 11:59:14 -0700

    changed rates to reflect 2015

--- src/rates.js
+++ src/rates.js
@@ -1,37 +1,29 @@
 ResidentialTaxRates = {
     main: {
-        "General": 2.7811,
-        "Debt": 0.1431,
-        "Police": 1.5172,
-        "Regional District": 0.2992,
-        "Hospitals" : 0.3014,
-        "Asst. Authority": 0.0596,
-        "M.F.A.": 0.0002,
+        "General, including Vic PD": 4.4414,
+        "School District": 1.7524,
+        "Capital Regional District": 0.2992,
+        "Capital Regional Hospital District" : 0.3014,
+        "BC Assessment": 0.0596,
+        "Municipal Finance Authority": 0.0002,
         "BC Transit":	0.2595 
     },
     optional: {
-        "BIA Hotel": 0.00032440,
-        "BIA Non-Hotel": 0.00064889,
-        "Boulevard":{"per sq. M":2.50},
-        "Sewer": {"per frontage M": 2.65}
     }
 };
 
 BusinessTaxRates = {
     main: {
-        "General": 5.1464,
-        "Debt": 0.2648,
-        "Police":2.8076,
-        "Regional District": 0.9346,
-        "Hospitals" : 1.0247,
-        "Asst. Authority": 0.1679,
-        "M.F.A.": 0.0002,
-        "BC Transit":	1.2586
+        "General, including Vic PD": 13.8749 ,
+        "School District": 5.8,
+        "Capital Regional District": 0.9346,
+        "Capital Regional Hospital District" : 0.7384,
+        "BC Assessment": 0.1679,
+        "Municipal Finance Authority": 0.0005,
+        "BC Transit": 1.2586  
     },
     optional: {
-        "BIA Hotel": 0.00032440,
-        "BIA Non-Hotel": 0.00064889,
-        "Boulevard":{"per sq. M":2.50},
-        "Sewer": {"per frontage M": 2.65}
+        "Downtown Victoria Business Association, Hotel": 0.00032440,
+        "Downtown Victoria Business Association, Non-Hotel": 0.00064889
     }
 };
