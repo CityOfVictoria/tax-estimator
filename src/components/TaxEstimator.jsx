121a8928d0cb752a2b4dce5503c9d021c05457bb
Delek Turner <dturner@victoria.ca>
2016-05-04 10:32:48 -0700

    undo extra changes that were not part of the last release

--- src/components/TaxEstimator.jsx
+++ src/components/TaxEstimator.jsx
@@ -270,45 +270,6 @@ var TaxEstimator = React.createClass({
     }
 });
 
-
-/* 
-    Tax Comparer
-    Compares two years of taxes (estimated)
-*/
-var CompareEstimates = React.createClass({
-getInitialState: function(){
-    return {
-    differences:{value:0, taxes:0}
-    };
-},
-sumTaxes: function(aorb){
-    var base = Object.getOwnPropertyNames(this.props.rates[aorb].baseRates).reduce(function(a,k){
-        return a + TaxCalculator.calculate(this.props.rates[aorb].baseRates[k], this.props.assessments[aorb], 0);
-    }.bind(this),0.0);
-
-    var optional = Object.getOwnPropertyNames(this.props.optionalRates).reduce(function(a,k){
-        return a + (this.props.included[k] ? TaxCalculator.calculate(this.props.rates[aorb].optionalRates[k], this.props.assessments[aorb], 0): 0);
-    }.bind(this),0.0);
-
-    return base + optional; 
-},
-calculateDifference: function(){
-    if(!!this.props.rates && !!this.props.rates.a){
-        var rs = Object.
-    }
-},
-handleValueChange: function(e){
-    this.setState({
-    assessmentA: (e.target.id=='a'?e.target.value:this.state.assessmentA),
-    assessmentB: (e.target.id=='b'?e.target.value:this.state.assessmentB),
-    
-    })
-},
-render: function(){
-    
-}
-});
-
 function insertAfter(referenceNode, newNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
